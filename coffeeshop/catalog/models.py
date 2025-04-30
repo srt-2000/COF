19e6f32d280126d968a34a348a1d9f694bbc645d
srt-2000 <srt2000888@gmail.ru>
2025-04-30 23:04:30 +0300

    first MR3 fixes after viewer, add docstrings and annotations, correct code with RUFF
     interactive rebase in progress; onto ee13919

--- coffeeshop/catalog/models.py
+++ coffeeshop/catalog/models.py
@@ -99,9 +99,8 @@ class AuxFieldModel(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=255, unique=True)
 
-    def __str__(self) -> str:
-        class Meta:
-            abstract = True
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
@@ -153,4 +152,3 @@ class ProductManufacture(AuxFieldModel):
     """
 
     pass
-
