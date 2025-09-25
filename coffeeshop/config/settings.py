e884f458f11ab7e5c189bf54298ec992553aeff1
srt-2000 <srt2000888@gmail.ru>
2025-09-25 23:29:32 +0300

    created manage command = loadtestdata changed service names and db host port in docker-compose.test.yml remove coffeeshop-collect-static and coffeeshop-load-test-data from docker-compose.yml

--- coffeeshop/config/settings.py
+++ coffeeshop/config/settings.py
@@ -91,8 +91,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         "ENGINE": environ.get("DATABASE_ENGINE"),
         "NAME": environ.get("DATABASE_NAME"),
         "USER": environ.get("DATABASE_USERNAME"),
